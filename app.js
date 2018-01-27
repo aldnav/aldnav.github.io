9833fd54dd6ced8418a0ed7dbb67ce62aa72745c
Aldrin Navarro <aldrin@channelfix.com>
2018-01-27 18:52:21 +0800

    Correct path from dir

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const port = process.env.A_PORT || 3000;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
-app.use('/static', express.static('public'));
+app.use('/static', express.static(__dirname + '/public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
